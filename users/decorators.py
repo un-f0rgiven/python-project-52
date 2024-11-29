9e663b24450a25e6d422b418a3d3da2913a8c643
un-f0rgiven <barr85@mail.ru>
2024-11-29 23:17:16 +1000

    add new file

---
+++ users/decorators.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.decorators import login_required as default_login_required
+from django.contrib import messages
+from django.shortcuts import redirect
+
+def login_required(view_func):
+    decorated_view_func = default_login_required(view_func)
+
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
+            return redirect('user_login')
+        return decorated_view_func(request, *args, **kwargs)
+
+    return wrapper
