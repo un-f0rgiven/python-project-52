2f474a245b2274e5725dc15aff2043fc1faeacc2
un-f0rgiven <barr85@mail.ru>
2024-12-04 16:02:34 +1000

    update file

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from .views import user_list, user_create, user_update, user_delete, user_login,
 urlpatterns = [
     path('', user_list, name='user_list'),
     path('create/', user_create, name='user_create'),
-    path('update/<int:pk>/', user_update, name='user_update'),
-    path('delete/<int:pk>/', user_delete, name='user_delete'),
+    path('<int:pk>/update/', user_update, name='user_update'),
+    path('<int:pk>/delete/', user_delete, name='user_delete'),
     path('login/', user_login, name='user_login'),
     path('logout/', user_logout, name='user_logout'),
 ]
