e3281247bf2d4bfa773c4360310799d6ce1fe5af
un-f0rgiven <barr85@mail.ru>
2024-12-04 15:39:36 +1000

    update file

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -104,20 +104,18 @@ print("DATABASE_URL:", DATABASE_URL)
 #     }
 # }
 
-if DEBUG:
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL),
 
-}
-
-print(f"dj_database: {dj_database_url.config(default=DATABASE_URL)}")
 print("DATABASES:", DATABASES)
 # Authentication
 
