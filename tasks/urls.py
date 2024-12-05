36477b95d5d071f89252a7c6a004701a363c91e6
un-f0rgiven <barr85@mail.ru>
2024-12-05 22:21:23 +1000

    update file

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,7 +3,8 @@ from .views import (
     task_list,
     task_create,
     task_update,
-    task_delete
+    task_delete,
+    task_show
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('create/', task_create, name='task_create'),
     path('<int:pk>/update/', task_update, name='task_update'),
     path('<int:pk>/delete/', task_delete, name='task_delete'),
+    path('<int:pk>/', task_show, name='task_show'),
 ]
