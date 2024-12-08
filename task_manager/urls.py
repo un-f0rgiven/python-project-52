993464b97e35b39f2f29a9b86f9044140d3907fc
un-f0rgiven <barr85@mail.ru>
2024-12-08 20:48:00 +1000

    update file

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.urls import include, path
+from django.urls import include, path
 
-# from task_manager.views import favicon_view, index
-# from users.views import user_login
+from task_manager.views import favicon_view, index
+from users.views import user_login
 
 urlpatterns = [
     path('', index, name='index'),
