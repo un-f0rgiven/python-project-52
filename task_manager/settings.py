314acf3ad97ec5afb74c6688d5548f517e07ec6f
un-f0rgiven <barr85@mail.ru>
2024-12-14 22:10:05 +1000

    delete file

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -111,6 +111,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
+            'ATOMIC_REQUESTS': True,
         }
     }
 
