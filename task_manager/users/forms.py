c513a79120bbf809ed8e266c9cfee6144ed31a9c
un-f0rgiven <barr85@mail.ru>
2024-12-15 00:11:44 +1000

    update file

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,14 +1,12 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 
-class UserCreateForm(forms.ModelForm):
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label='Подтверждение пароля',
-        widget=forms.PasswordInput
-    )
+class UserCreateForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, label='Имя')
+    last_name = forms.CharField(max_length=30, required=False, label='Фамилия')
 
     class Meta:
         model = User
@@ -20,27 +18,11 @@ class UserCreateForm(forms.ModelForm):
             'password2'
         ]
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get("password1")
-        password2 = cleaned_data.get("password2")
-
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Пароли не совпадают.")
-
-        if password1 and len(password1) < 3:
-            self.add_error(
-                'password2',
-                "Введённый пароль слишком короткий. "
-                "Он должен содержать как минимум 3 символа."
-            )
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
-        if commit:
-            user.save()
-        return user
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.exclude(pk=self.instance.pk).filter(username=username).exists():
+            raise ValidationError('Пользователь с таким именем существует.')
+        return username
 
 
 class UserUpdateForm(forms.ModelForm):
