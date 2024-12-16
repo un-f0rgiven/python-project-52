402fec814d8e91711b9ea8b72728c8a2bf5827d9
un-f0rgiven <barr85@mail.ru>
2024-12-16 16:04:56 +1000

    update file

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -4,6 +4,8 @@ from task_manager.labels.models import Label
 
 
 class LabelForm(forms.ModelForm):
+    name = forms.CharField(max_length=30, required=True, label='Имя')
+
     class Meta:
         model = Label
         fields = ['name']
