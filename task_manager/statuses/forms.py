cf477378155dbea13f58b0cd28d7e46a9a5fcbd4
un-f0rgiven <barr85@mail.ru>
2024-12-16 16:00:52 +1000

    update file

--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -4,6 +4,8 @@ from task_manager.statuses.models import Status
 
 
 class StatusForm(forms.ModelForm):
+    name = forms.CharField(max_length=30, required=True, label='Имя')
+
     class Meta:
         model = Status
         fields = ['name']
