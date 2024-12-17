14509b1db6874e319ee0d0591c0bae05f71abac4
un-f0rgiven <barr85@mail.ru>
2024-12-17 18:42:27 +1000

    update file

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,87 +1,75 @@
-from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
-from django.views.generic import (
-    CreateView,
-    DeleteView,
-    DetailView,
-    ListView,
-    UpdateView,
-)
-
-from task_manager.labels.models import Label
-from task_manager.statuses.models import Status
-from task_manager.tasks.filters import TaskFilter
+from django.views.generic.detail import DetailView
+from task_manager.base_views import BaseListView, BaseCreateView, BaseUpdateView, BaseDeleteView
 from task_manager.tasks.forms import TaskForm
 from task_manager.tasks.models import Task
-from task_manager.users.models import User
+from task_manager.tasks.filters import TaskFilter
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
 
-
-class TaskBaseView(LoginRequiredMixin):
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['statuses'] = Status.objects.all()
-        context['executors'] = User.objects.all()
-        context['labels'] = Label.objects.all()
-        return context
-
-
-class TaskListView(TaskBaseView, ListView):
+class TaskListView(BaseListView):
     model = Task
     template_name = 'tasks/task_list.html'
-    context_object_name = 'tasks'
-
-    def get_queryset(self):
-        tasks = Task.objects.all()
-        task_filter = TaskFilter(self.request.GET, queryset=tasks, user=self.request.user)
-        return task_filter.qs
+    success_url = '/tasks/'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filter'] = TaskFilter(self.request.GET, queryset=self.get_queryset(), user=self.request.user)
+        context['filter'] = TaskFilter()
         return context
 
-
-class TaskCreateView(TaskBaseView, CreateView):
+class TaskCreateView(BaseCreateView):
+    model = Task
     form_class = TaskForm
     template_name = 'tasks/task_create.html'
     success_url = '/tasks/'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        messages.success(self.request, 'Задача успешно создана')
         return super().form_valid(form)
 
+    def get_success_message(self):
+        return 'Задача успешно создана'
+    
+    def get_error_message(self):
+        return 'Невозможно создать задачу'
 
-class TaskUpdateView(TaskBaseView, UpdateView):
+class TaskUpdateView(BaseUpdateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/task_update.html'
     success_url = '/tasks/'
 
-    def form_valid(self, form):
-        messages.success(self.request, 'Задача успешно изменена.')
-        return super().form_valid(form)
+    def get_success_message(self):
+        return 'Задача успешно изменена.'
+    
+    def get_error_message(self):
+        return 'Невозможно изменить задачу'
 
-
-class TaskDeleteView(TaskBaseView, DeleteView):
+class TaskDeleteView(UserPassesTestMixin, BaseDeleteView):
     model = Task
     template_name = 'tasks/task_confirm_delete.html'
     success_url = '/tasks/'
 
+    def test_func(self):
+        task = self.get_object()
+        return task.author == self.request.user
+
     def dispatch(self, request, *args, **kwargs):
         task = self.get_object()
-        if task.author != request.user:
-            messages.error(request, 'Задачу может удалить только ее автор')
-            return redirect('task_list')
+        if not self.test_func():
+            messages.error(request, "Задачу может удалить только ее автор")
+            return redirect(reverse('task_list'))
         return super().dispatch(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        messages.success(request, 'Задача успешно удалена.')
-        return super().delete(request, *args, **kwargs)
+    def get_success_message(self):
+        return 'Задача успешно удалена.'
+    
+    def get_error_message(self):
+        return 'Невозможно удалить задачу'
 
-
-class TaskShowView(TaskBaseView, DetailView):
+class TaskShowView(DetailView):
     model = Task
     template_name = 'tasks/task_show.html'
+    context_object_name = 'task'
