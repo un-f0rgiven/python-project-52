03164ad17f055eb79160b7c09b4de6033a5b7568
un-f0rgiven <barr85@mail.ru>
2024-12-17 14:08:08 +1000

    update file

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -49,7 +49,7 @@ class StatusDeleteView(LoginRequiredMixin, DeleteView):
             messages.error(request, 'Невозможно удалить статус')
             return redirect('status_list')
 
-        messages.success(request, 'Статус успешно удален')
+        messages.success(request, 'Статус успешно удалена')
         return super().delete(request, *args, **kwargs)
 
 
