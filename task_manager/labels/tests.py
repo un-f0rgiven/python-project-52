ca2fd3bc479eed0d53d7d18f82990f87aacce69b
un-f0rgiven <barr85@mail.ru>
2024-12-18 00:26:43 +1000

    move file

---
+++ task_manager/labels/tests.py
@@ -0,0 +1,77 @@
+from django.contrib.messages import get_messages
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
+from task_manager.tasks.models import Task
+from task_manager.users.models import User
+
+
+class LabelViewsTests(TestCase):
+    fixtures = ['fixtures/initial_data.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.get(username='test_user')
+        self.client.login(username='test_user', password='testpass')
+        self.label = Label.objects.create(name='Important')
+
+    def test_label_list_view(self):
+        response = self.client.get(reverse('label_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'labels/label_list.html')
+        self.assertContains(response, self.label.name)
+
+    def test_label_create_view(self):
+        response = self.client.post(reverse('label_create'), {
+            'name': 'New Label'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Label.objects.filter(name='New Label').exists())
+        messages_list = list(get_messages(response.wsgi_request))
+        self.assertIn(
+            'Метка успешно создана.', [m.message for m in messages_list]
+        )
+
+    def test_label_update_view(self):
+        response = self.client.post(
+            reverse('label_update', args=[self.label.pk]),
+            {'name': 'Updated Label'}
+        )
+        self.label.refresh_from_db()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.label.name, 'Updated Label')
+        messages_list = list(get_messages(response.wsgi_request))
+        self.assertIn(
+            'Метка успешно изменена', [m.message for m in messages_list]
+        )
+
+    def test_label_delete_view(self):
+        response = self.client.post(
+            reverse('label_delete', args=[self.label.pk])
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Label.objects.filter(pk=self.label.pk).exists())
+        messages_list = list(get_messages(response.wsgi_request))
+        self.assertIn(
+            'Метка успешно удалена', [m.message for m in messages_list]
+        )
+
+    def test_label_delete_view_with_tasks(self):
+        status = Status.objects.create(name='New Status')
+        task = Task.objects.create(
+            name='Test Task', author=self.user, status=status
+        )
+        task.labels.add(self.label)
+
+        response = self.client.post(
+            reverse('label_delete', args=[self.label.pk])
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Label.objects.filter(pk=self.label.pk).exists())
+        messages_list = list(get_messages(response.wsgi_request))
+        self.assertIn(
+            'Невозможно удалить метку',
+            [m.message for m in messages_list]
+        )
---
+++ task_manager/users/tests.py
@@ -0,0 +1,112 @@
+from django.contrib.messages import get_messages
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from task_manager.users.models import User
+
+
+class UserViewsTests(TestCase):
+    fixtures = ['fixtures/initial_data.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.get(username='test_user')
+        self.another_user = User.objects.get(username='another_test_user')
+
+    def test_user_list_view(self):
+        self.client.login(username='test_user', password='password')
+        response = self.client.get(reverse('user_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/user_list.html')
+        self.assertContains(response, 'test_first_name')
+
+    def test_user_create_view(self):
+        response = self.client.post(reverse('user_create'), {
+            'username': 'new_user',
+            'first_name': 'New',
+            'last_name': 'User',
+            'password1': 'password',
+            'password2': 'password',
+        })
+        self.assertRedirects(response, reverse('user_login'))
+        self.assertTrue(User.objects.filter(username='new_user').exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(
+            str(messages[0]), 'Пользователь успешно зарегистрирован.'
+        )
+
+    def test_user_update_view(self):
+        self.client.login(username='test_user', password='testpass')
+        response = self.client.post(reverse(
+            'user_update', kwargs={'pk': self.user.pk}
+        ), {
+            'username': 'updated_user',
+            'first_name': 'Updated',
+            'last_name': 'User',
+            'password1': 'testpass',
+            'password2': 'testpass',
+        })
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'updated_user')
+        self.assertRedirects(response, reverse('user_list'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), 'Пользователь успешно изменен')
+    
+    def test_user_update_view_unauthorized(self):
+        self.client.login(username='another_test_user', password='password')
+        response = self.client.post(reverse(
+            'user_update', kwargs={'pk': self.user.pk}
+        ), {
+            'username': 'unauthorized_update',
+            'first_name': 'Unauthorized',
+            'last_name': 'User',
+        })
+        
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.username, 'test_user')
+        self.assertRedirects(response, reverse('user_list'))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(
+            str(messages[0]),
+            'У вас нет прав для изменения другого пользователя.'
+        )
+
+    def test_user_delete_other_user(self):
+        self.client.login(username='test_user', password='testpass')
+        response = self.client.post(reverse(
+            'user_delete', kwargs={'pk': self.another_user.pk})
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(pk=self.another_user.pk).exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(
+            str(messages[0]),
+            'У вас нет прав для изменения другого пользователя.'
+        )
+
+    def test_user_delete_self(self):
+        self.client.login(username='test_user', password='testpass')
+        response = self.client.post(reverse(
+            'user_delete', kwargs={'pk': self.user.pk})
+        )
+        self.assertRedirects(response, reverse('user_list'))
+        self.assertFalse(User.objects.filter(pk=self.user.pk).exists())
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), 'Пользователь успешно удален')
+
+    def test_user_login_view(self):
+        response = self.client.post(reverse('user_login'), {
+            'username': 'test_user',
+            'password': 'testpass',
+        })
+        self.assertRedirects(response, reverse('index'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_user_logout_view(self):
+        self.client.login(username='test_user', password='testpass')
+        response = self.client.post(reverse('user_logout'))
+        self.assertRedirects(response, reverse('index'))
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]), 'Вы разлогинены')
